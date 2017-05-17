a0e2a9445a62f0d4a9ec4429e8526768572840d1
Morse_Guo <nana.morse@outlook.com>
2017-05-17 21:35:58 +0800

    Update: render stage and add sprites

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
 import * as PIXI from 'pixi.js'
 
-console.log(PIXI);
+/** generate stage start */
+
+// generate renderer
+const renderer = PIXI.autoDetectRenderer(512, 512);
+
+// add renderer to the html document
+document.body.appendChild(renderer.view);
+
+// create a container called the 'stage'
+const stage = new PIXI.Container();
+
+renderer.backgroundColor = 0xFFFFFF;
+
+/** generate stage end */
+
+/** display sprite start */
+
+const superMarioPixelPngUrl = 'src/images/Super-Mario-Pixel.png';
+
+// load image to texture
+PIXI.loader.add(superMarioPixelPngUrl).load(() => {
+  // generate sprite
+  const sprite = new PIXI.Sprite(PIXI.loader.resources[superMarioPixelPngUrl].texture);
+
+  stage.addChild(sprite);
+
+  // render the stage
+  renderer.render(stage);
+});
+
